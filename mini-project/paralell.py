58492601ea79573c33ed23a8bb25ca4122519c13
tallosim <tallosi.mate@simonyi.bme.hu>
2023-02-28 14:23:54 +0100

    Update stage 1 of the mandelbrot set.

--- mini-project/paralell.py
+++ mini-project/paralell.py
@@ -2,39 +2,32 @@ import multiprocessing as mp
 import numpy as np
 from numba import jit
 
+
 @jit(nopython=True)
-def mandelbrot_chunk(c, M, I, T):
-    if c.shape != M.shape:
-        raise ValueError('c and M must have the same shape')
+def mandelbrot_chunk(c, I, T):
+    M = np.zeros(c.shape)
     
-    X_RES, Y_RES = c.shape
+    x_max, y_max = c.shape
     
-    for x in range(X_RES):
-        for y in range(Y_RES):
-            z = 0 + 0j
+    for x in range(x_max):
+        for y in range(y_max):
+            z = 0
             for i in range(I):
                 z = z**2 + c[x, y]
-                if T <= abs(z):
-                    M[x, y] = i / I
+                if abs(z) > T:
+                    M[x, y] = i
                     break
             else:
-                M[x, y] = 1
-                
+                M[x, y] = I
+    
     return M
-                
-                
-def mandelbrot_paralell(c, M, I, T, P):
-    if c.shape != M.shape:
-        raise ValueError('c and M must have the same shape')
-    
-    c_chunks = np.array_split(c, P, axis=0)
-    M_chunks = np.array_split(M, P, axis=0)    
-    
+
+def mandelbrot_paralell(c, I, T, P, chunk_size):
     pool = mp.Pool(processes=P)
 
-    results = [pool.apply_async(mandelbrot_chunk, args=(c_chunk, M_chunk, I, T)) for c_chunk, M_chunk in zip(c_chunks, M_chunks)]
-    
+    results = [pool.apply_async(mandelbrot_chunk, args=(c[i:i+chunk_size, :], I, T)) for i in range(0, c.shape[0], chunk_size)]
+
     pool.close()
     pool.join()
-    
-    M[:] = np.concatenate([result.get() for result in results], axis=0)
+
+    return results
